2085c5d85c871f58bb286413e30ab248f9464e4d
Vincent Liu <liuvincent818@gmail.com>
2026-09-11 14:10:52 -0400

    RAW Link and pyarrow

--- app_hw1.py
+++ app_hw1.py
@@ -7,7 +7,7 @@ st.write("11:35 Test")
 @st.cache_data
 def load_data(url):
     return pd.read_parquet(url)
-RAW = "https://github.com/liuvincent818-debug/CIVL-6962-HW1/blob/main/yellow_2024-03.parquet"
+RAW = "https://github.com/liuvincent818-debug/CIVL-6962-HW1/raw/refs/heads/main/yellow_2024-03.parquet"
 
 
 df = load_data(RAW)
