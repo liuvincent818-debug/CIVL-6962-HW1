447ea63c7452b0ece5b3b5384edca2cab4bc8bb9
Vincent Liu <liuvincent818@gmail.com>
2026-09-11 14:03:44 -0400

    Changed variable url to RAW

--- app_hw1.py
+++ app_hw1.py
@@ -7,8 +7,8 @@ st.write("11:35 Test")
 @st.cache_data
 def load_data():
     return pd.read_parquet(url)
-url = "https://github.com/liuvincent818-debug/CIVL-6962-HW1/blob/main/yellow_2024-03.parquet"
+RAW = "https://github.com/liuvincent818-debug/CIVL-6962-HW1/blob/main/yellow_2024-03.parquet"
 
 
-df = load_data(url)
+df = load_data(RAW)
 st.dataframe(df)
