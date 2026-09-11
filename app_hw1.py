e6bd0d44185ba9e00f879fa260c5e8d66528b559
Vincent Liu <liuvincent818@gmail.com>
2026-09-11 11:49:36 -0400

    Adding datafile

--- app_hw1.py
+++ app_hw1.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
 
 st.title("CIVL 6962 Dashboard")
 st.write("11:35 Test")
+
+df = pd.read_parquet("C:\\Users\\liuvi\\Downloads\\Machine Learning\\HW1 Database\\yellow_2024-03.parquet")
