0ba2bb2f4cf2c723bee4b04e9b4e4df48248b040
Vincent Liu <liuvincent818@gmail.com>
2026-09-11 14:02:10 -0400

    Loading data via URL

--- app_hw1.py
+++ app_hw1.py
@@ -4,4 +4,11 @@ import pandas as pd
 st.title("CIVL 6962 Dashboard")
 st.write("11:35 Test")
 
-df = pd.read_parquet("C:\\Users\\liuvi\\Downloads\\Machine Learning\\HW1 Database\\yellow_2024-03.parquet")
+@st.cache_data
+def load_data():
+    return pd.read_parquet(url)
+url = "https://github.com/liuvincent818-debug/CIVL-6962-HW1/blob/main/yellow_2024-03.parquet"
+
+
+df = load_data(url)
+st.dataframe(df)
