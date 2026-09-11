c56836189874d83f2e2d3b4f0695afcb81e44bf5
Vincent Liu <liuvincent818@gmail.com>
2026-09-11 11:21:35 -0400

    Test 3 for Main push

--- app_hw1.py
+++ app_hw1.py
@@ -2,4 +2,5 @@ import streamlit as st
 
 st.title("CIVL 6962 Dashboard")
 st.write("This works")
-st.write("This is to test the VSCode update function for GitHub")
+
+st.write("This is to test pushing to main from VSCODE")
